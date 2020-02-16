2960dd400f26e95d87f8bb64c69393704c7f004e
kakehi-binc <kakehi@binc.jp>
2020-02-16 14:48:03 +0900

    Pusher経由でPushを受信できるところまで確認した

--- push-notification/AppDelegate.swift
+++ push-notification/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +16,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { settings in
+            guard settings.authorizationStatus == .authorized else { return }
+            
+            if settings.alertSetting == .enabled {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+            
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -26,12 +38,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+         // 1. Convert device token to string
+         let tokenParts = deviceToken.map { data -> String in
+             return String(format: "%02.2hhx", data)
+         }
+         let token = tokenParts.joined()
+         // 2. Print device token to use for PNs payloads
+         print("debug0000 : Device Token: \(token)")
+     }
+
+     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+         // 1. Print out error if PNs registration not successful
+         print("debug0000 : Failed to register for remote notifications with error: \(error)")
+     }
+    
+    
+    // Push通知受信時とPush通知をタッチして起動したときに呼ばれる
+    // これが動いていない
+    func application(
+        application: UIApplication,
+        didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        
+        print("debug0000 : didReceiveRemoteNotification")
+        switch application.applicationState {
+        case .inactive:
+            // アプリがバックグラウンドにいる状態で、Push通知から起動したとき
+            print("debug0000 : inactive")
+            break
+        case .active:
+            // アプリ起動時にPush通知を受信したとき
+            print("debug0000 : active")
+            break
+        case .background:
+            // アプリがバックグラウンドにいる状態でPush通知を受信したとき
+            print("debug0000 : background")
+            break
+        }
     }
-
-
+    
+    
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // アプリが foreground の時に通知を受け取った時に呼ばれるメソッド
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void
+    ) {
+        print("debug0000 : userNotificationCenter")
+        if notification.request.trigger is UNPushNotificationTrigger {
+            print("debug0000 : This is UNPushNotificationTrigger")
+        }
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        print("debug0000 : userNotificationCenter didReceive")
+    }
+
+}
